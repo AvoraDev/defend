300ba32663f2592c53f2d216ad3837bfcc52764c
avRASDA <95705547+avRASDA@users.noreply.github.com>
2022-02-17 11:57:43 -0500

    Update player.js

--- js/templates/player.js
+++ js/templates/player.js
@@ -1,10 +1,10 @@
 class Player {
    //creates player with the following objects
-   constructor(x, y, radius, color, info) {
+   constructor(x, y, radius, color, info, settings) {
       this.x = x;
       this.y = y;
       this.radius = radius;
-      this.color = color;
+      this.color = {original: color.original, currently: color.currently};
       
       //gameplay information
       this.info = {
@@ -16,13 +16,18 @@ class Player {
          max: info.max,
          cooldown: {wait: info.cooldown.wait, enabled: info.cooldown.enabled}
       };
+      
+      this.settings = {
+         trailing: settings.trailing,
+         particles: settings.particles
+      };
    }
    
    //draws, or spawns, the player
    draw() {
       ctx.beginPath();
       ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      ctx.fillStyle = this.color;
+      ctx.fillStyle = this.color.currently;
       ctx.fill();
    }
 }
